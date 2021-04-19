0caf9bd43c2b20e68c3370fd13f654323ba78130
Ang Li <liang192120@outlook.com>
2021-04-19 14:36:10 +0800

    Restructure the repo

--- PCV/src/Control/Control.ino
+++ PCV/src/Control/Control.ino
@@ -3,26 +3,30 @@
 
 using namespace arduino_due::pwm_lib;
 
-// IMPORTANT: must connect each pin with corresponding valve!!!
+// IMPORTANT: must connect each pin with corresponding valve/sensor!!!
 #define INVALVE 6 // PWM, PIN 6
 #define OUTVALVE 7 // PWM, PIN 7
 #define AIRVALVE 8 // PWM, PIN 8
 #define O2VALVE 9 // PWM, PIN 9
+#define PRESSURE_SENSOR A1
 
 #define INHALATION 0
 #define EXHALATION 1
 
 #define PWM_PERIOD 100000 // 1kHz
+#define MAXIMUM_FLOW 256
 
 // System variables
 unsigned long referenceTime; // Reference system start time
 unsigned long startOfBreath; // Keep track of the start time of each breath
 
-// Inner circuit variables
+// User settings
 float PEEP;
-float targetPressure = 512; // In rawadc
+float targetPressure; // In rawadc
 float Tinhale; // Duration of a single inhalation
 float Texhale; // Duration of a single exhalation
+
+// Inner circuit variables
 boolean mode; // Used to identify which states the ventilator is in
 int inFlow; // Flow sensor in inhalation tube
 int outFlow; // Flow sensor in exhalation tube
@@ -31,28 +35,31 @@ int outO2; // O2 sensor in exhalation tube
 int pressure; // Pressure sensor in inner circuit
 
 // Outer circuit variables
-float FiO2 = 0.21; // Must be greater than room air oxygen concentration 0.21
+float FiO2 = 0.22; // Must be greater than room air oxygen concentration 0.21
 int outerPressure; // Pressure sensor in outer circuit
 
 // PID variables
 int totalError;
 int previousError;
-float kp = 10;
-float ki = 1;
+float kp;
+float ki;
 float kd;
 
 // PWM variables
-pwm<pwm_pin::PWML7_PC24> pwmInValve; // PIN 6
-pwm<pwm_pin::PWML6_PC23> pwmOutValve; // PIN 7
+pwm<pwm_pin::PWML7_PC24> pwmInValve; // PIN 6, inhalation valve
+pwm<pwm_pin::PWML6_PC23> pwmOutValve; // PIN 7, exhalation valve
+pwm<pwm_pin::PWML5_PC22> pwmAirValve; // PIN 8, air valve
+pwm<pwm_pin::PWML4_PC21> pwmO2Valve; // PIN 9, O2 valve
 
 /**
  * Function to compute PSI for 150PAAB5.
+ * 
  * @param rawadc raw analog signal
  * @return corresponding PSI
  */
 float adc2PSI150PA(int rawadc) {
-  // // Conversion equation from the data sheet
-  // float PSI = (rawadc * 3.3 / 1023.0 - 0.33) * 150 / 2.64;
+//  // Conversion equation from the data sheet
+//  float PSI = (rawadc * 3.3 / 1023.0 - 0.33) * 150 / 2.64;
 
   // Result from linear regression
   float PSI = 0.1737 * rawadc - 32.31021;
@@ -63,6 +70,7 @@ float adc2PSI150PA(int rawadc) {
 
 /**
  * Inversion function of adc2PSI150PA.
+ * 
  * @param PSI
  * @return corresponding adc   
  */
@@ -77,6 +85,7 @@ int PSI2adc150PA(float PSI) {
 /**
  * Given the target pressure value, this function compute the target flow to
  * to gain, which will be furtherly transformed into PWM signal by the effector.
+ * 
  * @param targetPressure current targeting pressure (in adc)
  * @return target flow
  */
@@ -89,18 +98,12 @@ int PIDController(int targetPressure) {
   targetFlow = (int)(0 + kp * error + ki * totalError + kd * (error - previousError));
   previousError = error;
 
-  /*******DEBUG******/
-  SerialUSB.print("current error: ");
-  SerialUSB.println(error);
-  SerialUSB.print("current target flow: ");
-  SerialUSB.println(targetFlow);
-
   return targetFlow;
 }
 
 /**
- * This function regulates INVALVE and OUTVALVE based on the targetFlow and
- * mode.
+ * This function regulates INVALVE and OUTVALVE based on the targetFlow.
+ * 
  * @param targetFlow current targeting flow
  */
 void Effector(int targetFlow) {
@@ -108,26 +111,23 @@ void Effector(int targetFlow) {
     pwmInValve.set_duty(0);
     pwmOutValve.set_duty(0);
   } else if (targetFlow > 0) {
-    int PWMSignal = targetFlow;
-    if (PWMSignal > 256) {
-      PWMSignal = 256;
+    if (targetFlow > MAXIMUM_FLOW) {
+      targetFlow = MAXIMUM_FLOW;
     }
 
-    float percentOpen = PWMSignal * 1.0 / 256;
+    float percentOpen = targetFlow * 1.0 / MAXIMUM_FLOW;
     pwmInValve.set_duty((int)(PWM_PERIOD * (0.6 + 0.3 * percentOpen)));
     pwmOutValve.set_duty(0);
   } else {
-    int PWMSignal = -targetFlow;
-
-    if (PWMSignal > 256) {
-      PWMSignal = 256;
+    targetFlow = -targetFlow;
+    if (targetFlow > MAXIMUM_FLOW) {
+      targetFlow = MAXIMUM_FLOW;
     }
 
-    float percentOpen = PWMSignal * 1.0 / 256;
-    pwmInValve.set_duty((int)(PWM_PERIOD * (0.6 + 0.3 * percentOpen)));
-    pwmOutValve.set_duty(0);
+    float percentOpen = targetFlow * 1.0 / MAXIMUM_FLOW;
+    pwmOutValve.set_duty((int)(PWM_PERIOD * (0.6 + 0.3 * percentOpen)));
+    pwmInValve.set_duty(0);
   }
-
 }
 
 /**
@@ -155,7 +155,7 @@ void exhalation() {
   }
 
   // TODO: set this trigger value
-  int trigger = 1024; // Determine whether to naturally breath out or to involve PID
+  int trigger = 0; // Determine whether to naturally breath out or to involve PID
   if (pressure - PEEP >= trigger) {
     breathOutEffector();
   } else {
@@ -169,8 +169,9 @@ void exhalation() {
  * minimum to enable a natural breathing out.
  */
 void breathOutEffector() {
-  // TODO: pass PWM signal to exhale valve for completely open it and limit
-  // inhale valve to practical minimum
+  pwmOutValve.set_duty(PWM_PERIOD);
+  // TODO: limit inhale valve to practical minimum
+  pwmInValve.set_duty(0);
 }
 
 /**
@@ -196,6 +197,8 @@ void setup() {
   // Set the valve PWM drivers
   pwmInValve.start(PWM_PERIOD, 0);
   pwmOutValve.start(PWM_PERIOD, 0);
+  pwmAirValve.start(PWM_PERIOD, 0);
+  pwmO2Valve.start(PWM_PERIOD, 0);
 
   // Set referenceTime as when the program starts
   referenceTime = millis();
@@ -203,38 +206,45 @@ void setup() {
 }
 
 /**
+ * Background loop.
+ */
+void loop() {
+  ;
+}
+
+/**
  * Main loop for inner circuit.
  */
-void loop() {// 50 Hz
-  // TODO:sensor value sampling
-  pressure = analogRead(A1);
-  SerialUSB.println("------------------------------");
-  SerialUSB.print("current pressure: ");
-  SerialUSB.println(pressure);
+void loop1(20) {// 50 Hz
+  // TODO: sensor value sampling
+  pressure = analogRead(PRESSURE_SENSOR);
 
   // Calculate current time in this cycle
   unsigned long systemTime = millis() - referenceTime;
   unsigned long cycleTime = systemTime - startOfBreath;
-//  if (cycleTime < Tinhale) {
-//    inhalation();
-//  } else if (cycleTime < Texhale) {
-//    exhalation();
-//  } else if (cycleTime > Texhale) {
-//    startOfBreath = systemTime; // Reset startOfBreath to start next breath
-//  }
-  inhalation(); //DEBUG: testing PID during inhalation
-
-  delay(1000);
+  if (cycleTime < Tinhale) {
+    inhalation();
+  } else if (cycleTime < Tinhale + Texhale) {
+    exhalation();
+  } else if (cycleTime >= Tinhale + Texhale) {
+    startOfBreath = systemTime; // Reset startOfBreath to start next breath
+  }
 }
 
-///**
-// * Main loop for outer circuit.
-// */
-//void loop2(1) { // 1kHz
-//  int oxygenFlow = 0; // TODO: set a standard HIGH ENOUGH value as airFlow is calculated based on this
-//  int airFlow;
-//
-//  airFlow = (int)((1 - FiO2) / (FiO2 - 0.21) * oxygenFlow);
-//
-//  // TODO: Send corresponding PWM signals to AIRVALVE and O2VALVE
-//}
+/**
+ * Main loop for outer circuit.
+ */
+void loop2(20) { // 50 Hz
+  // (Air Flow)/(Oxygen Flow) = (1 - FiO2)/(FiO2 - 0.21)
+  float airValvePercentOpen;
+  float O2ValvePercentOpen;
+  if (FiO2 < 0.605) { // Air Flow greater than Oxygen Flow
+    pwmAirValve.set_duty(PWM_PERIOD); // Completely open air valve
+    O2ValvePercentOpen = (FiO2 - 0.21)/(1 - FiO2);
+    pwmO2Valve.set_duty((int)(PWM_PERIOD * (0.6 + 0.3 * O2ValvePercentOpen)));
+  } else { // Oxygen Flow is greater than or equal to Air Flow
+    pwmO2Valve.set_duty(PWM_PERIOD); // Completely open O2 valve
+    airValvePercentOpen = (1 - FiO2)/(FiO2 - 0.21);
+    pwmAirValve.set_duty((int)(PWM_PERIOD * (0.6 + 0.3 * airValvePercentOpen)));
+  } 
+}
